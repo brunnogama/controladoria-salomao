07587deb8597c6ee5997715688323cf06f7bc6b0
Bruno Gama <30611127+brunnogama@users.noreply.github.com>
2025-12-25 13:02:50 -0300

    Create index.js

---
+++ src/hooks/index.js
@@ -0,0 +1,9 @@
+/**
+ * @file hooks/index.js
+ * @description Exportação centralizada de hooks customizados
+ * @version 1.4.0
+ */
+
+export { useClienteByCnpj } from './useClienteByCnpj';
+export { useContratos } from './useContratos';
+export { useDashboardMetrics } from './useDashboardMetrics';
