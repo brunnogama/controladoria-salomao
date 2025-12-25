66a6254f9d04ae005af5084e0db480cbc1ad9769
Bruno Gama <30611127+brunnogama@users.noreply.github.com>
2025-12-24 21:11:54 -0300

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,14 @@
-// Altere seu MainLayout.jsx para isso:
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../components/Sidebar'
 
 const MainLayout = () => {
   return (
-    <div className='flex h-screen bg-gray-50 overflow-hidden'> 
+    <div className='flex h-screen bg-gray-50 overflow-hidden'>
       {/* Sidebar fixa à esquerda */}
       <Sidebar />
 
-      {/* ÁREA DO CONTEÚDO PRINCIPAL - Adicionamos h-full e overflow-y-auto */}
+      {/* ÁREA DO CONTEÚDO PRINCIPAL */}
       <main className='flex-1 ml-64 h-full overflow-y-auto p-8 w-full transition-all duration-300'>
         <Outlet />
       </main>
