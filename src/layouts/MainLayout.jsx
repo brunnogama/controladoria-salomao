8a53aba15881b9e2d6e0beeb61282f02f1d8ab7f
Bruno Gama <30611127+brunnogama@users.noreply.github.com>
2025-12-24 22:55:23 -0300

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,7 +6,7 @@ const MainLayout = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex">
       <Sidebar />
-      <main className="flex-1 p-4 md:p-8 lg:ml-64 transition-all duration-300 pt-16 lg:pt-8">
+      <main className="flex-1 p-4 md:p-8 md:ml-64 transition-all duration-300 pt-16 md:pt-8">
         <div className="max-w-7xl mx-auto">
           <Outlet />
         </div>
