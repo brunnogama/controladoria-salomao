4006a6d70debd4d28fa749cae22282005494a4c8
Bruno Gama <30611127+brunnogama@users.noreply.github.com>
2025-12-25 13:51:32 -0300

    Update index.js

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,9 +1,10 @@
 /**
  * @file hooks/index.js
  * @description Exportação centralizada de hooks customizados
- * @version 1.4.0
+ * @version 1.4.3
  */
 
 export { useClienteByCnpj } from './useClienteByCnpj';
 export { useContratos } from './useContratos';
 export { useDashboardMetrics } from './useDashboardMetrics';
+export { useLogs } from './useLogs';
